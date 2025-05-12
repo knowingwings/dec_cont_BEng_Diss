d5410f3859e51bd6f0e961cd793782f95b450aa9
Tom Le Huray <thomas.lehuray@outlook.com>
2025-05-12 21:44:17 +0100

    Refactor CMakeLists.txt and setup.py for consistency and clarity by updating package names and removing unused code.

--- setup.py
+++ setup.py
@@ -1,25 +1,7 @@
-from setuptools import setup, find_packages
-import os
-from glob import glob
-from setuptools.dist import Distribution
-
-package_name = 'decentralized_control'
-
-# Override get_version to avoid version canonicalization
-Distribution.get_version = lambda self: '1.0'
+from setuptools import setup
 
 setup(
     name='dec_control',
-    packages=[package_name],
     package_dir={'': '.'},
-    data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/dec_control']),
-        ('share/dec_control', ['package.xml'])
-    ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='Tom Le Huray',
-    maintainer_email='thomas.lehuray@outlook.com',
-    description='Decentralized control for dual mobile manipulators',
-    license='TODO: License declaration'
+    packages=['decentralized_control']
 )
