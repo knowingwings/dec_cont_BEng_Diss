e52e359a0da04320aad5b428aa36810eebfc3320
Tom Le Huray <thomas.lehuray@outlook.com>
2025-05-12 21:33:47 +0100

    Update setup.py for PEP 440 compliance and correct package name

--- setup.py
+++ setup.py
@@ -6,20 +6,24 @@ package_name = 'dec_control'
 
 setup(
     name=package_name,
-    version='0.1.0',
-    packages=[package_name],
+    version='0.1.0.post0',  # Using PEP 440 compliant version
+    packages=['decentralized_control'],  # Use actual Python package name
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.*')),
+        (os.path.join('share', package_name, 'models'), glob('models/**/*.*', recursive=True)),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Tom Le Huray',
     maintainer_email='thomas.lehuray@outlook.com',
     description='Decentralized control for dual mobile manipulators',
-    license='TODO: License declaration',    tests_require=['pytest'],
+    license='TODO: License declaration',
+    tests_require=['pytest'],
     python_requires='>=3.8',
     entry_points={
         'console_scripts': [
