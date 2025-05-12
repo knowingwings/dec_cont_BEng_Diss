71d9d93121555a20af704e0c61aa3f8d2eb6182d
Tom Le Huray <thomas.lehuray@outlook.com>
2025-05-12 21:41:28 +0100

    Refactor setup.py for improved structure and clarity by consolidating setup arguments and removing unused data files.

--- setup.py
+++ setup.py
@@ -1,32 +1,25 @@
-from setuptools import find_packages
+from setuptools import setup, find_packages
 import os
 from glob import glob
+from setuptools.dist import Distribution
 
-package_name = 'decentralized_control'  # This should match your Python package directory name
+package_name = 'decentralized_control'
 
-setup_args = {
-    'name': 'dec_control',  # ROS package name
-    'version': '1.0',       # Simple version number
-    'packages': [package_name],
-    'package_dir': {'': '.'},
-    'include_package_data': True,
-    'zip_safe': True,
-    'author': 'Tom Le Huray',
-    'author_email': 'thomas.lehuray@outlook.com',
-    'maintainer': 'Tom Le Huray',
-    'maintainer_email': 'thomas.lehuray@outlook.com',
-    'description': 'Decentralized control for dual mobile manipulators',
-    'license': 'TODO: License declaration',
-    'install_requires': ['setuptools'],
-    'data_files': [
+# Override get_version to avoid version canonicalization
+Distribution.get_version = lambda self: '1.0'
+
+setup(
+    name='dec_control',
+    packages=[package_name],
+    package_dir={'': '.'},
+    data_files=[
         ('share/ament_index/resource_index/packages', ['resource/dec_control']),
-        ('share/dec_control', ['package.xml']),
-        (os.path.join('share', 'dec_control', 'launch'), glob('launch/*')),
-        (os.path.join('share', 'dec_control', 'config'), glob('config/*')),
-        (os.path.join('share', 'dec_control', 'models'), glob('models/**/*', recursive=True)),
-        (os.path.join('share', 'dec_control', 'worlds'), glob('worlds/*')),
+        ('share/dec_control', ['package.xml'])
     ],
-}
-
-from setuptools import setup
-setup(**setup_args)
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Tom Le Huray',
+    maintainer_email='thomas.lehuray@outlook.com',
+    description='Decentralized control for dual mobile manipulators',
+    license='TODO: License declaration'
+)
